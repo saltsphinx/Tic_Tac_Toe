33004e23a4f992a1891af62a5eb53eef3fb501bc
saltsphinx <tremar.thomas19@gmail.com>
2021-07-11 22:20:06 -0500

    Completed project.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+7/9/2021
 What is the problem?
 To build a Tic Tac Toe game that can be played by two people in the command line. 
 
@@ -9,10 +10,9 @@ What is the desired output? For the program to register the user(s) moves.
 
 Given
 
-Idea : Use a two dimenstional array. One array contain three others, representing the rows and columns of a tic tac toe game. Display to the cmd a grid like layout of which numbers to enter for a certain spot on the grid, for example : 1 2 3
-                                4 5 6
-                                7 8 9
-
+Idea : Use a two dimenstional array. One array contain three others, representing the rows and columns of a tic tac toe game. Display to the cmd a grid like layout of which numbers to enter for a certain spot on the grid, for example :  1 2 3
+                                                                4 5 6
+                                                                7 8 9
 To try to incorparate OOP, make each tic tac toe game an instance of the TicTacToe class that can record the results of each game played, and a new object made each game. 
 
 Where to start first?
@@ -28,3 +28,23 @@ There should be a Player class that records the players wins and loses.
 The players will be able to enter a number between 1 and 9, representing position on the grid. There needs to be a way to convert this into array indexes. (n = the users entered grid position, i is indexes in the containing array, and j is indexes the column array) First 1 needs to be subtracted from n (if 1 is n, it would be equal to 0. if 9 is n, it would be 8). Every multiple of three equals i+1, with i+2 being the highest possible. Any remainder will added to j. For example :
 n = 7, 2 multiples of three so i = 2, [2][0]
 7 - 6, 1 remainder so j = 1, [2][1]
+
+7/10/2021
+Find out a way to determine the winner
+I think the easiest thing to do first is to let the program know what the winning positions are. There are 8 possible winning aligments, and possible ties.
+Those being : Top Row([0][0], [0][1], [0][2]), Left Column([0][0], [1][0], [2][0]), Left Diagonal([0][0], [1][1], [2][2]), Right Diagonal([0][2], [1][1], [2][0])
+Middle Row([1][0], [1][1], [1][2]), Middle Column([0][1], [1][1], [2][1]), Bottom Row([2][0], [2][1], [2][1]), Right Column([0][2], [1][2], [2][2])
+
+Is a 2d array the best idea? This can all be converted to a single array. A bit of work will be lost, but calling a win will be much easier and programming with a single array would be simplier. 
+Where to start?
+First set up the classes. The GridPosition class idea was very cool, so I'll keep that
+Classes : TicTacToe, Player, GameBoard, GridPosition
+What are the class purposes? What will do they specfically?
+TicTacToe: The TicTacToe class is facilitate the entire playing experience. From starting the game to creating objects for other classes to recording the previous games played.
+Player: The Player class will record the player's wins and loses, as well as storing the players symbol, i.e. X or O.
+GameBoard: The GameBoard class will create instances that have an array(grid array) of 9 GridPosition objects, representing the 3x3 grid that tic tac toe is played on. It will have methods to check rather or not winning aligns have been formed. These positions(arrays of three integers) will be held within an array that is looped through each round. If the aligns are filled, but arent all matching characters then it will be removed from the array. Winning aligns are : [1, 2, 3], [1, 4, 7], [1, 5, 9], [3, 5, 7], [4, 5, 6], [2, 5, 8], [7, 8, 9], [3, 6, 9]
+The GameBoard will prevent reuse of the same grid positions and display the grid array to the cmd.
+GridPosition: The GridPosition class will create objects that are both meant for visiualing what positions are still avaliable to fill, but also provide functionality such as recording which grid positions are taken by who.
+
+7/11/2021
+Solve for the TicTacToe class. As I build out the tic tac toe class, make placeholder methods in other classes that will be built later.
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,88 +1,135 @@
 # frozen_string_literal: true
 
+# Handles object creation of other classes, method calling, game setup and play.
 class TicTacToe
-  @@record = []
   def initialize
-    @player_one = Player.new(gets.chomp, "X")
-    @player_two = Player.new(gets.chomp, "O")
+    @player_one = Player.new
+    @player_two = Player.new
     @game_board = GameBoard.new
-    @@record.push(self)
+    @@record = []
+    record_game
+    begin_game
   end
 
-  def play_round
-    @game_board.display_board
-    @game_board.game_move @player_one
-    @game_board.display_board
-    @game_board.game_move @player_two
-    @game_board.display_board
-  end
+  def begin_game
+    sleep 1
+    while @game_board.game_winner.nil?
+      puts "#{@player_one.name}'s turn..."
+      @game_board.turn @player_one
+      break unless @game_board.game_winner.nil?
 
-  def display_board
-    @game_board.display_board
-  end
-
-  def self.display_records
-    @@record
-  end
-end
-
-class Player
-  attr_reader :name, :character
-  attr_accessor :wins, :loses
-  def initialize n, c
-    @name = n
-    @character = c
-  end
-end
-
-class GameBoard
-  attr_accessor :game_board
-  @@game_boards = []
-  def initialize
-    num_pad_first = 1
-    num_pad_last = 9
-    @game_board = [[GridPosition.new(1), GridPosition.new(2), GridPosition.new(3)],[GridPosition.new(4), GridPosition.new(5), GridPosition.new(6)],[GridPosition.new(7), GridPosition.new(8), GridPosition.new(9)]]
-    @@game_boards.push(self)
-  end
-
-  def display_board
-    puts "\t\t\t\t\t\t #{@game_board[0][0].character} | #{@game_board[0][1].character} | #{@game_board[0][2].character}\n\t\t\t\t\t\t--- --- ---\n\t\t\t\t\t\t #{@game_board[1][0].character} | #{@game_board[1][1].character} | #{@game_board[1][2].character}\n\t\t\t\t\t\t--- --- ---\n\t\t\t\t\t\t #{@game_board[2][0].character} | #{@game_board[2][1].character} | #{@game_board[2][2].character}"
-  end
-
-  def game_move (player)
-    position = check_correct_move(gets.chomp)
-    if position.between?(1,9)
-      position -= 1
-      container_index = 0
-      while (position - 3) >= 0
-        position -= 3
-        container_index += 1
-      end
-      puts "[#{container_index}][#{position}]"
-      unless @game_board[container_index][position].filled
-        @game_board[container_index][position].character = player.character
-        @game_board[container_index][position].filled = true
-      else
-        puts 'Turn skipped.'
-      end
+      puts "#{@player_two.name}'s turn..."
+      @game_board.turn @player_two
     end
+    call_game
   end
 
-  private
-  def check_correct_move (move)
-    move = move.to_i
-    if move.between?(1, 9)
-      move
+  def call_game
+    puts "#{@game_board.game_winner.name} wins!"
+    @game_board.game_winner.wins += 1
+    sleep 1
+    new_game
+  end
+
+  def new_game
+    puts "Begin a new game?\ny/n"
+    if gets.chomp.match?(/y/i)
+      puts 'Beginning new game...'
+      @game_board = GameBoard.new
+      record_game
+      begin_game
     else
-      0
+      puts "Game ended.\n#{@player_one.name}'s wins: #{@player_one.wins}."
+      puts "#{@player_two.name}'s wins: #{@player_two.wins}."
+    end
+  end
+
+  def record_game
+    @@record.push(@game_board)
+  end
+end
+
+# Handles the two users information
+class Player
+  attr_accessor :name, :character, :wins, :loses
+
+  def initialize
+    puts 'Enter your name.'
+    @name = gets.chomp
+    puts 'Enter a letter to represent you.'
+    while user_input = gets.chomp
+      if user_input.match?(/[a-z|!|@|#|$|%|^|&|*|(|)|+|=]/i)
+        @character = user_input.match(/[a-z]/i)
+        break
+      else
+        puts 'Enter a letter.'
+      end
+    end
+    @wins = 0
+  end
+end
+
+# Is where the game is played. Displays 3x3 grid to console, and determines if alignments are created
+class GameBoard
+  attr_reader :game_winner
+
+  def initialize
+    @board = Array.new((1..9).map { |n| GridPosition.new(n) })
+    @game_winner = nil
+    @alignments = [[1, 2, 3], [1, 4, 7], [1, 5, 9], [3, 5, 7], [4, 5, 6], [2, 5, 8], [7, 8, 9], [3, 6, 9]]
+  end
+
+  def show_board
+    puts " #{@board[0].character} | #{@board[1].character} | #{@board[2].character}"
+    puts '--- --- ---'
+    puts " #{@board[3].character} | #{@board[4].character} | #{@board[5].character}"
+    puts '--- --- ---'
+    puts " #{@board[6].character} | #{@board[7].character} | #{@board[8].character}"
+  end
+
+  def turn(player)
+    show_board
+    user_input = gets.chomp.to_i.to_s
+    if correct_move? user_input
+      @board[user_input.match(/[1-9]/)[0].to_i - 1].affect player
+      check_board player
+    else
+      puts 'That positions taken! Pick another.'
+      sleep 1
+      turn player
+    end
+  end
+
+  def correct_move?(input)
+    if input.match?(/[1-9]/) && @board[input.match(/[1-9]/)[0].to_i - 1].filled == false
+      true
+    else
+      false
+    end
+  end
+
+  def check_board(player)
+    @alignments.each do |alignment|
+      if alignment.all? { |i| @board[i - 1].filled == true } && alignment.all? { |i| @board[i - 1].player == player }
+        @game_winner = player
+        show_board
+      end
     end
   end
 end
 
+# Creates instances that are used in the grid array. Prevents reuse of grid items, and is used for visuals.
 class GridPosition
-  attr_accessor :character, :filled
-  def initialize c
-    @character = c
+  attr_accessor :character, :player, :filled
+
+  def initialize(char)
+    @character = char
     @filled = false
+  end
+
+  def affect(player)
+    @character = player.character
+    @player = player
+    @filled = true
   end
 end
